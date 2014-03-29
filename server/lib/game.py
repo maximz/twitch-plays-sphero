3b6d4387c4a98a49bfefde85c4c664f4d15bd9d1
Maxim Zaslavsky <maxim@maximzaslavsky.com>
2014-03-29 18:42:59 -0400

    My lib/ modifications were not being tracked, oops

---
+++ server/lib/game.py
@@ -0,0 +1,29 @@
+#import win32api
+#import win32con
+import time
+
+class Game:
+
+    keymap = {
+        'up': 0x30,
+        'down': 0x31,
+        'left': 0x32,
+        'right': 0x33,
+        'a': 0x34,
+        'b': 0x35,
+        'start': 0x36,
+        'select': 0x37
+    }
+
+    def get_valid_buttons(self):
+        return [button for button in self.keymap.keys()]
+
+    def is_valid_button(self, button):
+        return button in self.keymap.keys()
+
+    def button_to_key(self, button):
+        return self.keymap[button]
+
+    def push_button(self, button):
+		# TODO: send button press to sphero
+		print 'pushed button', button # debug
